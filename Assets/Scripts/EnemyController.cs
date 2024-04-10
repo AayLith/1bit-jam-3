34d183e504d5cb9a28ca057c8f550c96b16c782d
turtletennis <stephenjenkins42@gmail.com>
2024-04-10 20:00:10 +0100

    Enemy chases player!

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,17 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+
+enum EnemyState
+{
+    Idle,
+    SearchingForPlayer,
+    ChasingPlayer,
+    FleeingFromPlayer,
+    Hiding
+}
+
 
 public class EnemyController : MonoBehaviour
 {
+    [SerializeField]
+    private float secondsPerState = 2;
     private Animator animator;
     [SerializeField]
+    private EnemyState currentState = EnemyState.Idle;
+    [SerializeField]
+    private float visionRange = 50;
+    private float secondsInCurrentState = 0;
+    private PlayerController player;
+    private SpriteRenderer spriteRenderer;
+    private bool facingLeft = false;
+    [SerializeField]
     private float movementSpeed;
+    [SerializeField]
+    private int playerLayer = 7;
+    [SerializeField]
+    private Vector3 playerCentreOffset = new Vector2(0,-0.25f);
+    private Health health;
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         SetAnimationState(AnimationState.Idle);
+        player = FindObjectOfType<PlayerController>();
+        health = GetComponent<Health>();
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void SetAnimationState(AnimationState animationState)
@@ -19,9 +51,120 @@ public class EnemyController : MonoBehaviour
         animator.SetInteger("state", (int) animationState);
     }
 
+    void FixedUpdate()
+    {
+        UpdateState();
+        switch (currentState)
+        {
+            case EnemyState.Idle:
+                BeIdle();
+                break;
+            case EnemyState.SearchingForPlayer:
+                SearchForPlayer();
+                break;
+            case EnemyState.ChasingPlayer:
+                ChasePlayer();
+                break;
+            case EnemyState.FleeingFromPlayer:
+                Flee();
+                break;
+            case EnemyState.Hiding:
+                Hide();
+                break;
+        }
+    }
+
+    private void Hide()
+    {
+
+    }
+
+    private void Flee()
+    {
+
+    }
+    private void ChasePlayer()
+    {
+        var hit = FindPlayer();
+        if (hit != null)
+        {
+            var deltaPos = hit.Value.point - (Vector2) transform.position;
+            facingLeft = Mathf.Sign(deltaPos.x) == -1;
+            var xSpeed = Mathf.Sign(deltaPos.x) * movementSpeed;
+            rb.velocity =   Vector3.Lerp(rb.velocity,new Vector2(xSpeed,rb.velocity.y),0.8f);
+        }
+    }
+    private void SearchForPlayer()
+    {
+
+    }
+
+    private void BeIdle()
+    {
+
+    }
+
+    private RaycastHit2D? FindPlayer()
+    {
+        var deltaPosition = player.transform.position + playerCentreOffset - gameObject.transform.position;
+        var distance = deltaPosition.magnitude;
+
+        if (distance < visionRange)
+        {
+            int layerMask = 1 << playerLayer;
+            ContactFilter2D filter2D = new ContactFilter2D();
+            filter2D.layerMask = layerMask;
+            var hits = Physics2D.RaycastAll(transform.position,deltaPosition.normalized,visionRange,layerMask);
+            Debug.DrawRay(transform.position,deltaPosition.normalized*visionRange,Color.red);
+            if(hits.Length > 0)
+            {
+                foreach(var hit in hits)
+                {
+                    var hitPlayer = hit.collider.gameObject.GetComponentInParent<PlayerController>();
+                    UnityEngine.Debug.Log($"{hitPlayer.gameObject.name} == {player.gameObject.name}");
+                    if(hitPlayer !=null)
+                    {
+                        UnityEngine.Debug.Log("Found player!");
+                        return hit;
+                    }
+                }
+            }
+        }
+        return null;
+        
+    }
+    [SerializeField]
+    private float fleeHealthFraction = 0.25f;
+    private void UpdateState()
+    {   FindPlayer();
+        secondsInCurrentState += Time.fixedDeltaTime;
+        if(secondsInCurrentState > secondsPerState)
+        {
+            var hit = FindPlayer();
+            if(hit != null)
+            {
+                UnityEngine.Debug.Log("Found player");
+                if(health.CurrentHealth / health.MaxHealth > fleeHealthFraction)
+                {   // can see player, healthy, chase
+                    currentState = EnemyState.ChasingPlayer;
+                }
+                else
+                {   // can see player, low health, flee
+                    currentState = EnemyState.FleeingFromPlayer;
+                }
+                
+            }
+            else
+            {   // can't see player
+                currentState = EnemyState.Idle;
+                UnityEngine.Debug.Log("Could not find player");
+            }
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        spriteRenderer.flipX = facingLeft;
     }
 }
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -6,7 +6,15 @@ public class Health : MonoBehaviour
 {
     [SerializeField]
     private float maxHealth;
+    public float MaxHealth
+    {
+        get { return maxHealth; }
+    }
     private float currentHealth;
+    public float CurrentHealth
+    {
+        get { return currentHealth; }
+    }
     public delegate void OnDeath();
     public OnDeath onDeath;
     // Start is called before the first frame update
